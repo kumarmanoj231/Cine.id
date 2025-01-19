1d12ae84adbee5d4ef7a223be3b3b073e1fad53e
kumarmanoj231 <lodhamanoj383@gmail.com>
2025-01-19 22:16:26 +0530

    styling views

--- models/Movie.js
+++ models/Movie.js
@@ -1,45 +1,48 @@
 const mongoose = require("mongoose");
 
 const movieSchema = mongoose.Schema({
-    title : {
-        type : String,
-        required : true,
-    },
-    releaseYear : {
-        type : Number,
-        required : true,
-    },
-    genre : {
-        type : [String],
-        required : true,
-       
-    },
-    director : {
-        type : [String],
-        required : true,
-    },
-    cast : {
-        type : [String],
-        required : true,
-    },
-    duration : {
-        type : Number,
-        required : true,
-    },
-    rating : {
-        type : Number,
-        default : 0.0,
-        required : true,
-    },
-    posterUrl : {
-        type : String,
-        required : true,
-    },
-    synopsis : {
-        type : String,
-        required : true,
-    }
-
+  title: {
+    type: String,
+    required: true,
+  },
+  year: {
+    type: Number,
+    required: true,
+  },
+  genres: {
+    type: [{ type: String, required: true }],
+  },
+  directors: {
+    type: [
+      {
+        type: String,
+        required: true,
+      },
+    ],
+  },
+  cast: {
+    type : [
+        {type : String,
+            required : true,
+        }
+    ]
+  },
+  duration: {
+    type: Number,
+    required: true,
+  },
+  rating: {
+    type: Number,
+    required: true,
+  },
+  url: {
+    type: String,
+    required: true,
+  },
+  synopsis: {
+    type: String,
+    required: true,
+  },
 });
 
 const Movie = mongoose.model("Movie", movieSchema);
